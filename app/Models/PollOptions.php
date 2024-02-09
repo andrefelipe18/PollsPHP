cfd31acdd3c0e60ae73262f1c4700465dedeca59
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-02-08 22:06:14 -0300

    Poll Options and relations to Poll Model

--- app/Models/PollOptions.php
+++ app/Models/PollOptions.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Poll extends Model
+class PollOptions extends Model
 {
     use HasFactory;
 
@@ -14,8 +14,8 @@ class Poll extends Model
         'title',
     ];
 
-    public function options(): HasMany
+    public function poll(): BelongsTo
     {
-        return $this->hasMany(PollOptions::class);
+        return $this->belongsTo(Poll::class);
     }
 }
